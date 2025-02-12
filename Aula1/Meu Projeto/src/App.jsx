20568ecb84eb22028369fc0b7a1c929b49e6c98e
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-02-12 02:48:07 -0300

    UPDATED AULA 1

--- Aula1/Meu Projeto/src/App.jsx
+++ Aula1/Meu Projeto/src/App.jsx
@@ -1,10 +1,12 @@
+import React from "react";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 
-function App() {
+const App = () => {
+  return <>
+  <Header/>
+  <Main/>;
+  </>
+};
 
-  return (
-    <>
-    </>
-  )
-}
-
-export default App
+export default App;
