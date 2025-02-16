8b5e37d8d9740aa7d91519b561e6343fb6592fcf
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-02-15 22:48:20 -0300

    UPDATED AULA 4 BACK END

--- Aula 4/Meu Projeto/back-end/api/server.js
+++ Aula 4/Meu Projeto/back-end/api/server.js
@@ -1,30 +1,27 @@
 // API significa Application Programming Interface
 import express from "express";
+import { artistArray } from "../../front-end/src/assets/database/artists.js";
+import { songsArray } from "../../front-end/src/assets/database/songs.js";
 const app = express();
 
-const {
+import {
   checkHeaderMiddleware,
   httpsSecurityMiddleware,
   discordLogs,
-  autoLoader,
-} = require("npm-package-nodejs-utils-lda");
+} from "npm-package-nodejs-utils-lda";
+import { criarArtista, criarSong, getArtists, getSongs } from "./connect.js";
 // const hostname = "127.0.0.1"; só local
 // const hostname = "0.0.0.0"; Bind na placa de rede
 // const hostname = "::"; bind ipv4 e ipv6 pra fora
 const hostname = "::";
-const porta = process.env.PINGOBRAS_SG_PORTA;
-const dinamicPort = porta || 8080;
+const porta = process.env.PORTA;
+const dinamicPort = porta || 3002;
 
-const date = new Date();
-const dia = date.getDate().toString().padStart(2, "0") - 1;
-const dia7 = (date.getDate() - 7).toString().padStart(2, "0") - 1;
-const mes = (date.getMonth() + 1).toString().padStart(2, "0");
-const ano = date.getFullYear();
 const setCacheHeaders = (req, res, next) => {
   console.log("Adicionando header Cache de 1 dia");
-  const cacheDuration = 60 * 60 * 24; // 1 dia em segundos
-  res.set("Cache-Control", `public, max-age=${cacheDuration}`);
-  res.set("Cache-time", cacheDuration);
+  const cacheDuration = 60 * 24; // 1 dia em segundos
+  res.set("Cache-Control", `public, max-age=${60 *cacheDuration}`);
+  res.set("Cache-time", 60 *cacheDuration);
   next();
 };
 app.use(express.urlencoded({ extended: true }));
@@ -32,19 +29,30 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(cacheMiddleware);
 app.use(setCacheHeaders);
 app.use(httpsSecurityMiddleware);
-app.use(ddosModule().express);
 
 app.use(express.json());
-autoEditais(dia, dia7, mes, ano);
 checkHeaderMiddleware(app);
-app.use(pingobrasSystems);
 
-// Carrega dinamicamente todos os módulos de rota
-autoLoader(app);
+app.get("/artist",async (req, res) => {
+  res.send(await getArtists());
+});
+
+app.get("/songs",async (req, res) => {
+  res.send(await getSongs());
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
 var server = app.listen(dinamicPort, hostname, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log("Servidor rodando em http://%s:%s", hostname, port);
   console.log("IP Obtido: http://%s:%s", host, port);
-  discordLogs("START", `Servidor rodando em http://${hostname}:${port}`);
+  discordLogs(
+    "START SERVER BACKEND SPOTIFY",
+    `Servidor rodando em http://${hostname}:${port}`
+  );
 });
