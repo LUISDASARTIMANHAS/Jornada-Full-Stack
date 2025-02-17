3959559d2e2293d499c579ace8327f8ae2dd0356
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-02-16 21:21:12 -0300

    ajustes para o deploy

--- Deploy/back-end/api/server.js
+++ Deploy/back-end/api/server.js
@@ -9,9 +9,11 @@ import {
   discordLogs,
 } from "npm-package-nodejs-utils-lda";
 import { criarArtista, criarSong, getArtists, getSongs } from "./connect.js";
+import path, { dirname } from "path";
 // const hostname = "127.0.0.1"; só local
 // const hostname = "0.0.0.0"; Bind na placa de rede
 // const hostname = "::"; bind ipv4 e ipv6 pra fora
+const __dirname = path.resolve();
 const hostname = "::";
 const porta = process.env.PORTA;
 const dinamicPort = porta || 3002;
@@ -19,8 +21,8 @@ const dinamicPort = porta || 3002;
 const setCacheHeaders = (req, res, next) => {
   console.log("Adicionando header Cache de 1 dia");
   const cacheDuration = 60 * 24; // 1 dia em segundos
-  res.set("Cache-Control", `public, max-age=${60 *cacheDuration}`);
-  res.set("Cache-time", 60 *cacheDuration);
+  res.set("Cache-Control", `public, max-age=${60 * cacheDuration}`);
+  res.set("Cache-time", 60 * cacheDuration);
   next();
 };
 app.use(express.urlencoded({ extended: true }));
@@ -33,18 +35,23 @@ app.use(cors());
 app.use(express.json());
 checkHeaderMiddleware(app);
 
-app.get("/artists",async (req, res) => {
+app.get("/api/artists", async (req, res) => {
   res.send(await getArtists());
 });
 
-app.get("/songs",async (req, res) => {
+app.get("/api/songs", async (req, res) => {
   res.send(await getSongs());
 });
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+});
 
 var server = app.listen(dinamicPort, hostname, function () {
   var host = server.address().address;
--- Deploy/front-end/src/api/api.js
+++ Deploy/front-end/src/api/api.js
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import axios from "axios";
 
-const url = "http://localhost:3001";
+const { NODE_ENV } = process.env;
+const url = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
 const resArtists = await axios.get(`${url}/artists`);
 const resSongs = await axios.get(`${url}/songs`);
 export const artistArray = resArtists.data;
